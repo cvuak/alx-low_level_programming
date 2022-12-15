341603858513193558783ed081fcbbc63cb806d7
cvuak <ovuakporiechampion@gmail.com>
2022-12-15 09:26:43 -0800

    r

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,13 +12,13 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-
 	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -27,7 +27,7 @@ void print_remaining_days(int month, int day, int year)
 		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day -
-			311, year);
+			31, year);
 		}
 		else
 		{
@@ -36,4 +36,3 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 }
-
