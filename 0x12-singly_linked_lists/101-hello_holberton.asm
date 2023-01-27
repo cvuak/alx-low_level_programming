58c2d5f1b77756368f8fed679a9a258dc3929ad1
cvuak <ovuakporiechampion@gmail.com>
2023-01-26 18:05:40 -0800

    upd

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,14 @@
-#include <stdio.h>
-/**
- * bmain - function executed before main
- * Return: no return.
- */
-void __attribute__((constructor)) bmain()
-{
-	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
-}
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+	mov eax, 0
+	ret
